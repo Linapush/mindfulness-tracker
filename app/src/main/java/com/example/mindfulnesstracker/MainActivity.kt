ee882fd568462944419ae9e88a92c683f34e3ab4
Linapush <linapush2002@gmail.com>
2024-07-02 18:37:44 +0300

    add pages

--- app/src/main/java/com/example/mindfulnesstracker/MainActivity.kt
+++ app/src/main/java/com/example/mindfulnesstracker/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.mindfulnesstracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -13,39 +15,51 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mindfulnesstracker.ui.theme.MindfulnessTrackerTheme
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             MindfulnessTrackerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background,
-                ) {
-                    Greeting("Android")
-                }
+                HabbitApp()
             }
         }
     }
 }
-
-@Composable
-fun Greeting(
-    name: String,
-    modifier: Modifier = Modifier,
-) {
-    Button(
-        onClick = {
-            throw RuntimeException("Test Crash")
-        },
-    ) { Text(text = "Crash") }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MindfulnessTrackerTheme {
-        Greeting("Android")
-    }
-}
+//class MainActivity : ComponentActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContent {
+//            MindfulnessTrackerTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background,
+//                ) {
+//                    Greeting("Android")
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//@Composable
+//fun Greeting(
+//    name: String,
+//    modifier: Modifier = Modifier,
+//) {
+//    Button(
+//        onClick = {
+//            throw RuntimeException("Test Crash")
+//        },
+//    ) { Text(text = "Crash") }
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    MindfulnessTrackerTheme {
+//        Greeting("Android")
+//    }
+//}
