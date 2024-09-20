6719f139dfbcff4924abccb413dd833f36d1dc20
Linapush <linapush2002@gmail.com>
2024-09-20 20:04:47 +0300

    fix HabbitNavigation: add user_id, fix onHabitLabelClick in composable List Screen;

--- app/src/main/java/com/example/mindfulnesstracker/ui/HabitNavigation.kt
+++ app/src/main/java/com/example/mindfulnesstracker/ui/HabitNavigation.kt
@@ -7,10 +7,12 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.example.mindfulnesstracker.USER_ID
 import com.example.mindfulnesstracker.ui.screens.AddScreen
 import com.example.mindfulnesstracker.ui.screens.ListScreen
 import com.example.mindfulnesstracker.ui.screens.ProgressScreen
 import com.example.mindfulnesstracker.ui.screens.SplashScreen
+import java.net.URLEncoder
 
 private const val SPLASH_SCREEN = "splash"
 private const val LIST_SCREEN = "list"
@@ -18,14 +20,10 @@ private const val PROGRESS_SCREEN = "progress"
 private const val ADD_SCREEN = "add"
 private const val ARG_HABIT_ID = "habitId"
 
-// NavController - компонент, управляющий навигацией между различными destination
-// NavHost - это контейнер, который содержит набор различных экранов (composables), которые могут быть отображены в зависимости от текущего состояния навигации
-// Используется для определения навигационного графа и управления переходами между экранами
-
 @Composable
 fun HabitNavigation() {
-    // сохранение состояния навигационного контроллера между перерисовками Composable-функции
     val navigator = rememberNavController()
+
     NavHost(
         navController = navigator,
         startDestination = SPLASH_SCREEN,
@@ -39,13 +37,14 @@ fun HabitNavigation() {
         }
         composable(LIST_SCREEN) {
             ListScreen(
-                onItemClick = { navigator.navigate("$PROGRESS_SCREEN/$it") },
+                userId = USER_ID,
+                onHabitLabelClick = { habitId -> navigator.navigate("$PROGRESS_SCREEN/${URLEncoder.encode(habitId, "UTF-8")}") },
                 onAddClick = { navigator.navigate(ADD_SCREEN) },
             )
         }
         composable(
             "$PROGRESS_SCREEN/{$ARG_HABIT_ID}",
-            listOf(navArgument(ARG_HABIT_ID) { type = NavType.IntType }),
+            listOf(navArgument(ARG_HABIT_ID) { type = NavType.StringType }),
         ) { backStackEntry ->
             val id = backStackEntry.arguments?.getInt(ARG_HABIT_ID)
 
